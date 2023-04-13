cc1e0139afd6a419b0f8a7892214544446e54b00
tshivakumar <shivakumar.tambali@gmail.com>
2023-04-13 16:33:14 +0530

    Added config files

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Issues.MixProject do
   def project do
     [
       app: :issues,
+      escript: escript_config(),
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
@@ -19,10 +20,17 @@ defmodule Issues.MixProject do
   end
 
   # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  def deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
+      {:httpoison, "~> 2.0"},
+      {:poison, "~> 5.0"},
+      {:ex_doc, "~> 0.29.4"},
+      {:earmark, "~> 1.4"}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
+  end
+
+  defp escript_config do
+    [main_module: Issues.CLI]
   end
 end
